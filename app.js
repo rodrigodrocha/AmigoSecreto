cc7e79fdb0796e30b42224238250165f74066dd0
rodrigodrocha <rodrigo.rocha9229@gmail.com>
2025-02-06 19:36:18 -0300

    criar a função adicionarAmigo do botão com funções auxiliares para limpar campo e acessar o item do html

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function limparCampo(id) {
+        let apagar = pegarElementoApartirDoID(id);
+        apagar.value = '';
+}
+
+function pegarElementoApartirDoID(id){
+    return document.getElementById(id);
+}
+
+//Função do botão adicionar amigo na lista.
+function adicionarAmigo(){
+    let amigo;
+    amigo = pegarElementoApartirDoID('amigo').value;
+    
+    if (amigo !== '') {
+        amigos.push(amigo);
+    }else{
+        alert('Por favor, insira um nome.');
+    }
+    limparCampo('amigo');
+    //console.log(amigos);
+}
