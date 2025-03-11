75b636af2d1a3bc90f82545a1c4092ccf0ef12a6
rodrigodrocha <rodrigo.rocha9229@gmail.com>
2025-03-11 14:42:06 -0300

    Implementação de uma função para atualizar a lista de amigos na Página web criando a tag li e inserindo ela no html

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
+//Limpando um campo apartir de um id
 function limparCampo(id) {
         let apagar = pegarElementoApartirDoID(id);
         apagar.value = '';
 }
-
+// pegando um elemento apartir do ID 
 function pegarElementoApartirDoID(id){
     return document.getElementById(id);
 }
@@ -21,5 +22,17 @@ function adicionarAmigo(){
         alert('Por favor, insira um nome.');
     }
     limparCampo('amigo');
-    //console.log(amigos);
+    atualizarLista();
+}
+function atualizarLista() {
+    console.log(amigos)
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        const novoAmigo = document.createElement("li");
+        novoAmigo.textContent = amigos[i];
+        listaAmigos.appendChild(novoAmigo);
+    }
+    
 }
